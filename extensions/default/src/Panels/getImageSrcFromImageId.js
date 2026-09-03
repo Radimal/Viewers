887b859da7a6464b303b3da6231874b3be2b13c7
Roy Davis <roy@radimal.ai>
2026-09-03 17:09:41 -0400

    fix: render monochrome thumbnails with correct polarity

--- extensions/default/src/Panels/getImageSrcFromImageId.js
+++ extensions/default/src/Panels/getImageSrcFromImageId.js
@@ -6,9 +6,12 @@ function getImageSrcFromImageId(cornerstone, imageId) {
   return new Promise((resolve, reject) => {
     const canvas = document.createElement('canvas');
     const startTime = Date.now();
-    
+    const imagePixelModule = cornerstone.metaData.get('imagePixelModule', imageId);
+    const useCPURendering =
+      imagePixelModule?.photometricInterpretation === 'MONOCHROME1';
+
     cornerstone.utilities
-      .loadImageToCanvas({ canvas, imageId, thumbnail: true })
+      .loadImageToCanvas({ canvas, imageId, thumbnail: true, useCPURendering })
       .then(imageId => {
         resolve(canvas.toDataURL());
       })
